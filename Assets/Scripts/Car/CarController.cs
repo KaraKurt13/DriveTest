6da3666849ae7f35ed92e92169c63bdae5f1bd97
KaraKurt <penguin.ners@gmail.com>
2024-12-21 23:27:32 +0200

    Basic car control

---
+++ Assets/Scripts/Car/CarController.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Car
+{
+    public class CarController : MonoBehaviour
+    {
+        [SerializeField]
+        private WheelCollider _frontLeftWheelCollider, _frontRightWheelCollider, _rearLeftWheelCollider, _rearRightWheelCollider;
+
+        [SerializeField]
+        private Transform _frontLeftWheelTransform, _frontRightWheelTransform, _rearLeftWheelTransform, _rearRightWheelTransform;
+
+        private const float _maxTorque = 1000, _maxSteerAngle = 30f, _brakeForce = 3000f;
+
+        private float _currentSteerAngle, _targetSteerAngle = 0f;
+
+        private float _horizontalInput, _verticalInput;
+        private bool _brakeInput;
+
+        public bool IsDrifting { get; private set; }
+
+        private void Update()
+        {
+            GetInput();
+            UpdateWheels();
+        }
+
+        private void FixedUpdate()
+        {
+            Debug.Log(_brakeInput);
+            HandleMotor();
+            HandleSteering();
+            HandleBrake();
+            HandleDrifting();
+        }
+
+        private void GetInput()
+        {
+            _verticalInput = Input.GetAxis("Vertical");
+            _horizontalInput = Input.GetAxis("Horizontal");
+            _brakeInput = Input.GetKey(KeyCode.Space);
+        }
+
+        private void HandleMotor()
+        {
+            var motorTorque = _verticalInput * _maxTorque;
+            _rearLeftWheelCollider.motorTorque = motorTorque;
+            _rearRightWheelCollider.motorTorque = motorTorque;
+            HandleBrake();
+        }
+
+        private void HandleSteering()
+        {
+            _currentSteerAngle = _horizontalInput * _maxSteerAngle;
+            _frontLeftWheelCollider.steerAngle = _currentSteerAngle;
+            _frontRightWheelCollider.steerAngle = _currentSteerAngle;
+        }
+
+        private void HandleBrake()
+        {
+            var brakeForce = _brakeInput ? _brakeForce : 0f;
+
+            _frontLeftWheelCollider.brakeTorque = brakeForce;
+            _frontRightWheelCollider.brakeTorque = brakeForce;
+            _rearLeftWheelCollider.brakeTorque = brakeForce;
+            _rearRightWheelCollider.brakeTorque = brakeForce;
+        }
+
+        private void HandleDrifting()
+        {
+
+        }
+
+        private void UpdateWheels()
+        {
+            UpdateSingleWheel(_rearLeftWheelCollider, _rearLeftWheelTransform);
+            UpdateSingleWheel(_rearRightWheelCollider, _rearRightWheelTransform);
+            UpdateSingleWheel(_frontLeftWheelCollider, _frontLeftWheelTransform);
+            UpdateSingleWheel(_frontRightWheelCollider, _frontRightWheelTransform);
+        }
+
+        private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+        {
+            Vector3 position;
+            Quaternion rotation;
+            wheelCollider.GetWorldPose(out position, out rotation);
+            wheelTransform.rotation = rotation;
+            wheelTransform.position = position;
+        }
+
+        private void LerpWheelRotation()
+        {
+
+        }
+    }
+}
