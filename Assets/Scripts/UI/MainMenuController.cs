14711da41c03a66de715ed05bf47c96840a8ce55
KaraKurt <penguin.ners@gmail.com>
2024-12-22 02:23:46 +0200

    TMP + basic main menu

---
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _mainPanel, _modeSelectionPanel;
+
+        public ShopMenuComponent ShopMenuComponent;
+
+        public SettingsComponent SettingsComponent;
+
+        public void DrawMailPanel()
+        {
+            _mainPanel.SetActive(true);
+        }
+
+        public void HideMainPanel()
+        {
+            _mainPanel.SetActive(false);
+        }
+
+        public void DrawModeSelection()
+        {
+            _modeSelectionPanel.SetActive(true);
+        }
+
+        public void HideModeSelection()
+        {
+            _modeSelectionPanel.SetActive(false);
+        }
+    }
+}
